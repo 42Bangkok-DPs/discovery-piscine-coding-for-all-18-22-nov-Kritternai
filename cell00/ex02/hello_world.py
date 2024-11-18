a9670b18317f91a49cccaf5c3885e5ffed37d2ff
kbbk-x <krittanai67@gmail.com>
2024-11-18 19:34:21 +0700

    Displaying a name (name.py)

--- cell00/ex02/hello_world.py
+++ cell00/ex02/hello_world.py
@@ -1,2 +1,3 @@
 # hello_world.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell00/ex02/hello_world.py)
+
 print("Hello World")
---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+# name.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell01/ex00/name.py)
+first_name = "Krittanai" ; last_name = "Boonnoy"
+print(first_name, last_name)
