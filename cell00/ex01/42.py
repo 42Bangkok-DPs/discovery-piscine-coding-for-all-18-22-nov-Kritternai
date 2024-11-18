4060d64d5f35e741786f3d9a2a6d1b1e88c3f061
kbbk-x <krittanai67@gmail.com>
2024-11-18 12:34:01 +0700

    Hello ! And modified cell00/ex01/42.py

---
+++ cell00/ex01/42.py
@@ -0,0 +1,2 @@
+# 42.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell00/ex01/42.py)
+print("42")
---
+++ cell00/ex02/hello_world.py
@@ -0,0 +1,2 @@
+# hello_world.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell00/ex02/hello_world.py)
+print("Hello World")
