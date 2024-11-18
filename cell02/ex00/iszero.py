f8cde33ea71a921cd32cccba08123ebe823387e4
kbbk-x <krittanai67@gmail.com>
2024-11-19 02:08:53 +0700

    fix some bug and Cell 2 Update

---
+++ cell02/ex00/iszero.py
@@ -0,0 +1,6 @@
+#iszero.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell02/ex00/iszero.py)
+number = float(input(""))
+if number == 0 :
+    print("This number is equal to zero.")
+else :
+    print("This number is different from zero.")
