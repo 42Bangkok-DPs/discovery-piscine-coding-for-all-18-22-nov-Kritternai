bfafb27834b9549c92e013718c0868c1327187f6
kbbk-x <krittanai67@gmail.com>
2024-11-19 16:04:38 +0700

    Fix bug

--- cell01/ex01/name.py
+++ cell01/ex01/name.py
@@ -1,5 +1,4 @@
 # name.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell01/ex01/name.py)
 first_name = "Krittanai" ; last_name = "Boonnoy"
-whole_name = last_name + " " + first_name
-
+whole_name = first_name + " " + last_name
 print(whole_name, end="\n")
--- cell01/ex03/whatsyourname.py
+++ cell01/ex03/whatsyourname.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
 # whatsyourname.py (discovery-piscine-coding-for-all-18-22-nov-Kritternai/cell01/ex03/whatsyourname.py)
-first_name = input("Hey, what's your first name? : ")
-last_name = input("And your last name? : ")
+
+first_name = input("Hey, what's your first name? : ").strip()
+last_name = input("And your last name? : ").strip()
 
 print(f"Well, pleased to meet you, {first_name} {last_name}.")
